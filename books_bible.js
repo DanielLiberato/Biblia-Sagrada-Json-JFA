10f5211928de204eba587b2bd89357506818d6ee
DanielLiberato <daniel.bacellar022@gmail.com>
2023-10-25 17:18:52 -0300

    v1 Biblia

---
+++ books_bible.js
@@ -0,0 +1,331 @@
+var booksbible = [
+    {
+        "book_id": 1,
+        "book_name": "Gênesis",
+        "chapters": 50
+    },
+    {
+        "book_id": 2,
+        "book_name": "Êxodo",
+        "chapters": 40
+    },
+    {
+        "book_id": 3,
+        "book_name": "Levítico",
+        "chapters": 27
+    },
+    {
+        "book_id": 4,
+        "book_name": "Números",
+        "chapters": 36
+    }, {
+        "book_id": 5,
+        "book_name": "Deuteronômio",
+        "chapters": 34
+    },
+    {
+        "book_id": 6,
+        "book_name": "Josué",
+        "chapters": 24
+    },
+    {
+        "book_id": 7,
+        "book_name": "Juízes",
+        "chapters": 21
+    },
+    {
+        "book_id": 8,
+        "book_name": "Rute",
+        "chapters": 4
+    },
+    {
+        "book_id": 9,
+        "book_name": "1 Samuel",
+        "chapters": 31
+    },
+    {
+        "book_id": 10,
+        "book_name": "2 Samuel",
+        "chapters": 24
+    },
+    {
+        "book_id": 11,
+        "book_name": "1 Reis",
+        "chapters": 22
+    },
+    {
+        "book_id": 12,
+        "book_name": "2 Reis",
+        "chapters": 25
+    },
+    {
+        "book_id": 13,
+        "book_name": "1 Crônicas",
+        "chapters": 29
+    },
+    {
+        "book_id": 14,
+        "book_name": "2 Crônicas",
+        "chapters": 36
+    },
+    {
+        "book_id": 15,
+        "book_name": "Esdras",
+        "chapters": 10
+    },
+    {
+        "book_id": 16,
+        "book_name": "Neemias",
+        "chapters": 13
+    },
+    {
+        "book_id": 17,
+        "book_name": "Ester",
+        "chapters": 10
+    },
+    {
+        "book_id": 18,
+        "book_name": "Jó",
+        "chapters": 42
+    },
+    {
+        "book_id": 19,
+        "book_name": "Salmos",
+        "chapters": 150
+    },
+    {
+        "book_id": 20,
+        "book_name": "Provérbios",
+        "chapters": 31
+    },
+    {
+        "book_id": 21,
+        "book_name": "Eclesiastes",
+        "chapters": 12
+    },
+    {
+        "book_id": 22,
+        "book_name": "Cânticos",
+        "chapters": 8
+    },
+    {
+        "book_id": 23,
+        "book_name": "Isaías",
+        "chapters": 66
+    },
+    {
+        "book_id": 24,
+        "book_name": "Jeremias",
+        "chapters": 52
+    },
+    {
+        "book_id": 25,
+        "book_name": "Lamentações",
+        "chapters": 5
+    },
+    {
+        "book_id": 26,
+        "book_name": "Ezequiel",
+        "chapters": 48
+    },
+    {
+        "book_id": 27,
+        "book_name": "Daniel",
+        "chapters": 12
+    },
+    {
+        "book_id": 28,
+        "book_name": "Oseias",
+        "chapters": 14
+    },
+    {
+        "book_id": 29,
+        "book_name": "Joel",
+        "chapters": 3
+    },
+    {
+        "book_id": 30,
+        "book_name": "Amós",
+        "chapters": 9
+    },
+    {
+        "book_id": 31,
+        "book_name": "Obadias",
+        "chapters": 1
+    },
+    {
+        "book_id": 32,
+        "book_name": "Jonas",
+        "chapters": 4
+    },
+    {
+        "book_id": 33,
+        "book_name": "Miquéias",
+        "chapters": 7
+    },
+    {
+        "book_id": 34,
+        "book_name": "Naum",
+        "chapters": 3
+    },
+    {
+        "book_id": 35,
+        "book_name": "Habacuque",
+        "chapters": 3
+    },
+    {
+        "book_id": 36,
+        "book_name": "Sofonias",
+        "chapters": 3
+    },
+    {
+        "book_id": 37,
+        "book_name": "Ageu",
+        "chapters": 2
+    },
+    {
+        "book_id": 38,
+        "book_name": "Zacarias",
+        "chapters": 14
+    },
+    {
+        "book_id": 39,
+        "book_name": "Malaquias",
+        "chapters": 4
+    },
+    {
+        "book_id": 40,
+        "book_name": "Mateus",
+        "chapters": 28
+    },
+    {
+        "book_id": 41,
+        "book_name": "Marcos",
+        "chapters": 16
+    },
+    {
+        "book_id": 42,
+        "book_name": "Lucas",
+        "chapters": 24
+    },
+    {
+        "book_id": 43,
+        "book_name": "João",
+        "chapters": 21
+    },
+    {
+        "book_id": 44,
+        "book_name": "Atos",
+        "chapters": 28
+    },
+    {
+        "book_id": 45,
+        "book_name": "Romanos",
+        "chapters": 16
+    },
+    {
+        "book_id": 46,
+        "book_name": "1 Coríntios",
+        "chapters": 16
+    },
+    {
+        "book_id": 47,
+        "book_name": "2 Coríntios",
+        "chapters": 13
+    },
+    {
+        "book_id": 48,
+        "book_name": "Gálatas",
+        "chapters": 6
+    },
+    {
+        "book_id": 49,
+        "book_name": "Efésios",
+        "chapters": 6
+    },
+    {
+        "book_id": 50,
+        "book_name": "Filipenses",
+        "chapters": 4
+    },
+    {
+        "book_id": 51,
+        "book_name": "Colossenses",
+        "chapters": 4
+    },
+    {
+        "book_id": 52,
+        "book_name": "1 Tessalonicenses",
+        "chapters": 5
+    },
+    {
+        "book_id": 53,
+        "book_name": "2 Tessalonicenses",
+        "chapters": 3
+    },
+    {
+        "book_id": 54,
+        "book_name": "1 Timóteo",
+        "chapters": 6
+    },
+    {
+        "book_id": 55,
+        "book_name": "2 Timóteo",
+        "chapters": 4
+    },
+    {
+        "book_id": 56,
+        "book_name": "Tito",
+        "chapters": 3
+    },
+    {
+        "book_id": 57,
+        "book_name": "Filemom",
+        "chapters": 1
+    },
+    {
+        "book_id": 58,
+        "book_name": "Hebreus",
+        "chapters": 13
+    },
+    {
+        "book_id": 59,
+        "book_name": "Tiago",
+        "chapters": 5
+    },
+    {
+        "book_id": 60,
+        "book_name": "1 Pedro",
+        "chapters": 5
+    },
+    {
+        "book_id": 61,
+        "book_name": "2 Pedro",
+        "chapters": 3
+    },
+    {
+        "book_id": 62,
+        "book_name": "1 João",
+        "chapters": 5
+    },
+    {
+        "book_id": 63,
+        "book_name": "2 João",
+        "chapters": 1
+    },
+    {
+        "book_id": 64,
+        "book_name": "3 João",
+        "chapters": 1
+    },
+    {
+        "book_id": 65,
+        "book_name": "Judas",
+        "chapters": 1
+    },
+    {
+        "book_id": 66,
+        "book_name": "Apocalipse",
+        "chapters": 22
+    }
+];
---
+++ harpa_crista.js
@@ -0,0 +1,30 @@
+var harpa_crista = [
+    {
+        "hino_id": 1,
+        "hino_name": "Chuvas de Graça",
+        "autor_tradutor": "J.R José Rodrigues",
+        "verses": [
+            "Deus prometeu com certeza <br> Chuvas de graça mandar; <br> Ele nos dá fortaleza, <br> E ricas bênçãos sem par",
+            "Chuvas de graça, <br> Chuvas pedimos, Senhor; <br> Manda-nos chuvas constantes, <br> Chuvas do Consolador.",
+            "Cristo nos tem concedido <br> O santo Consolador, <br> De plena paz nos enchido, <br> Para o reinado do amor.",
+            "Chuvas de graça, <br> Chuvas pedimos, Senhor; <br> Manda-nos chuvas constantes, <br> Chuvas do Consolador.",
+            "Dá-nos, Senhor, amplamente, <br> Teu grande gozo e poder; <br> Fonte de amor permanente, <br> Põe dentro de nosso ser.",
+            "Chuvas de graça, <br> Chuvas pedimos, Senhor; <br> Manda-nos chuvas constantes, <br> Chuvas do Consolador.",
+            "Faze os teus servos piedosos, <br> Dá-lhes virtude e valor, <br> Dando os teus dons preciosos, <br> Do santo Preceptor.",
+            "Chuvas de graça, <br> Chuvas pedimos, Senhor; <br> Manda-nos chuvas constantes, <br> Chuvas do Consolador.",
+        ]
+    },
+    {
+        "hino_id": 2,
+        "hino_name": "Saudosa Lembrança",
+        "autor_tradutor": "A.N Adriano Nobbre",
+        "verses": [
+            "Oh! que saudosa lembrança <br> Tenho de ti, ó Sião, <br> Terra que eu tanto amo, <br> Pois és do meu coração. <br> Eu para ti voarei, <br> Quando o Senhor meu voltar; <br> Pois Ele foi para o céu, <br> E breve vem me buscar.",
+            "Sim, eu porfiarei por essa terra de além; <br> E lá terminarei as muitas lutas de aquém; <br> Lá está meu bom Senhor, ao qual eu desejo ver; <br> Ele é tudo p’ra mim, e sem Ele não posso viver.",
+            "Bela, mui bela, é a esperança, <br> Dos que vigiam por ti, <br> Pois eles recebem força, <br> Que só se encontra ali; <br> Os que procuram chegar <br> Ao teu regaço, ó Sião, <br> Livres serão de pecar <br> E de toda a tentação.",
+            "Sim, eu porfiarei por essa terra de além; <br> E lá terminarei as muitas lutas de aquém; <br> Lá está meu bom Senhor, ao qual eu desejo ver; <br> Ele é tudo p’ra mim, e sem Ele não posso viver.",
+            "Diz a Sagrada Escritura, <br> Que são formosos os pés <br> Daqueles que boas novas <br> Levam para os infiéis; <br> E, se tão belo é falar <br> Dessas grandezas, aqui, <br> Que não será o gozar <br> A graça que existe ali!",
+            "Sim, eu porfiarei por essa terra de além; <br> E lá terminarei as muitas lutas de aquém; <br> Lá está meu bom Senhor, ao qual eu desejo ver; <br> Ele é tudo p’ra mim, e sem Ele não posso viver.",
+        ]
+    }
+]
